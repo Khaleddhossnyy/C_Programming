e33546dc600a81e24e3c940b92ea794fc4abbfc9
khaled hossny <khaledhossny73@gmail.com>
2022-11-29 11:04:33 +0200

    first commit

---
+++ 03-Avr_Drivers/02-HAL_Layer/02-Keypad_Driver/KEYPAD_Config.h
@@ -0,0 +1,24 @@
+/*
+ * 02-KEYPAD_Config.h
+ *
+ *  Created on: Nov 26, 2022
+ *      Author: user
+ */
+
+#ifndef HAL_LAYER_02_KEYPAD_DRIVER_02_KEYPAD_CONFIG_H_
+#define HAL_LAYER_02_KEYPAD_DRIVER_02_KEYPAD_CONFIG_H_
+//------------------------------------------------------------
+#define KPD_u8_ROW_0_PIN  DIO_u8_PIN_0
+#define KPD_u8_ROW_1_PIN  DIO_u8_PIN_1
+#define KPD_u8_ROW_2_PIN  DIO_u8_PIN_2
+#define KPD_u8_ROW_3_PIN  DIO_u8_PIN_3
+
+#define KPD_u8_ROWS_PORT GROUP_A
+#define KPD_u8_COLS_PORT GROUP_A
+
+#define KPD_u8_COL_0_PIN  DIO_u8_PIN_4
+#define KPD_u8_COL_1_PIN  DIO_u8_PIN_5
+#define KPD_u8_COL_2_PIN  DIO_u8_PIN_6
+#define KPD_u8_COL_3_PIN  DIO_u8_PIN_7
+//------------------------------------------------------------
+#endif /* 02_HAL_LAYER_02_KEYPAD_DRIVER_02_KEYPAD_CONFIG_H_ */
---
+++ 03-Avr_Drivers/02-HAL_Layer/02-Keypad_Driver/KEYPAD_Private.h
@@ -0,0 +1,15 @@
+/*
+ * 02-KEYPAD_Private.h
+ *
+ *  Created on: Nov 26, 2022
+ *      Author: user
+ */
+
+#ifndef HAL_LAYER_02_KEYPAD_DRIVER_02_KEYPAD_PRIVATE_H_
+#define HAL_LAYER_02_KEYPAD_DRIVER_02_KEYPAD_PRIVATE_H_
+
+//-----------------------------------------------------
+//-----------------------------------------------------
+
+
+#endif /* 02_HAL_LAYER_02_KEYPAD_DRIVER_02_KEYPAD_PRIVATE_H_ */
