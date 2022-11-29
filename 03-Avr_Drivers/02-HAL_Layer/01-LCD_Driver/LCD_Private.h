fc54f8e4535a041703bc566a895a915e51e5a09e
khaled hossny <khaledhossny73@gmail.com>
2022-11-29 11:03:15 +0200

    first commit

---
+++ 03-Avr_Drivers/02-HAL_Layer/01-LCD_Driver/LCD_Private.h
@@ -0,0 +1,18 @@
+/*
+ * LCD_Private.h
+ *
+ *  Created on: Nov 25, 2022
+ *      Author: user
+ */
+
+#ifndef HAL_LAYER_01_LCD_DRIVER_LCD_PRIVATE_H_
+#define HAL_LAYER_01_LCD_DRIVER_LCD_PRIVATE_H_
+//--------------------------------------------
+#define LCD_u8_RS_Cmd    0
+#define LCD_u8_RS_Data   1
+
+#define LCD_u8_RW_Write  0
+#define LCD_u8_RW_Read   1
+
+//--------------------------------------------
+#endif /* 02_HAL_LAYER_01_LCD_DRIVER_LCD_PRIVATE_H_ */
